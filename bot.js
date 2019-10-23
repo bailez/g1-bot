2b4f7d02df5a47d4d2d5e5f8d3da3ffdb042400b
bailez <bailez@usp.br>
2019-10-22 23:19:03 -0300

    retweet function

--- bot.js
+++ bot.js
@@ -2,7 +2,14 @@ const config = require('./config')
 const twit = require('twit')
 const Twitter = new twit(config)
 
-
-
-
 console.log('Hello World')
+
+functon retweet()
+{
+    let params={
+        q:'#got',
+        result_type: 'recent',
+        count: 100
+    }
+    
+}
