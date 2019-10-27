8d96fe74e0d327a84917dd0fee47b2ba31027218
bailez <bailez@usp.br>
2019-10-26 23:01:51 -0300

    creates tweet func

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,24 @@
-const functions = require('firebase-functions');
+var functions = require('firebase-functions');
 var twit = require('twit');
 var config = require('./config.js');
+var XMLHttpRequest = require('xhr2');
+var dictionary = require('./dictionary.js')
+
+var admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 var Twitter = new twit(config);
 
-var admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)    
+toPost = 'test'
 
-exports.showTwitter = functions.https.onRequest((request, response) => {
-    response.send(Twitter);
+exports.tweetBest = functions.https.onRequest((req, res) => {
+    
+    Twitter.post('statuses/update', {status: toPost},  function(error, tweet, response){
+        if(error){
+            console.log(error);
+        }
+        console.log(tweet);  // Tweet body.
+        console.log(response);
+        return res.send('New tweet, YAY!');  // Raw response object.
+      
     });
+})
