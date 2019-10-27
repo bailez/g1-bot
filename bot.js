d23653c1cdc893018f14430b1d79fd8a16880ad6
bailez <bailez@usp.br>
2019-10-26 21:49:47 -0300

    reset firebase

--- bot.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config')
-var Twitter = require('twitter')
-var client = new Twitter(config)
-client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
-    if(error) throw error;
-    console.log(tweet);  // Tweet body.
-    console.log(response);  // Raw response object.
-  });
