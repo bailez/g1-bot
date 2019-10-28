3d367bd75916013562023ed4399af396dfbba398
bailez <bailez@usp.br>
2019-10-27 23:49:06 -0300

    updte indx

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,14 @@
 var functions = require('firebase-functions');
 var twit = require('twit');
 var config = require('./config.js');
-var XMLHttpRequest = require('xhr2');
-var dictionary = require('./dictionary.js')
 
 var admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 var Twitter = new twit(config);
 
-toPost = 'test'
+toPost = 'ovo mexido muito dificil de comprar as vezes teste fea usp'
 
 exports.tweetBest = functions.https.onRequest((req, res) => {
     
-    Twitter.post('statuses/update', {status: toPost},  function(error, tweet, response){
-        if(error){
-            console.log(error);
-        }
-        console.log(tweet);  // Tweet body.
-        console.log(response);
-        return res.send('New tweet, YAY!');  // Raw response object.
-      
-    });
+    Twitter.post('statuses/update', {status: toPost})
 })
